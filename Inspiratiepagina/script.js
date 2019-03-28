b555982ed42b7713c5d1b7747aa111181ee0f485
Timmie <timmie.vanengelen@student.fontys.nl>
2019-03-28 12:29:26 +0100

    Versie 1.1
    Eerste versie af

--- Inspiratiepagina/script.js
+++ Inspiratiepagina/script.js
@@ -2,14 +2,6 @@ function openoverlay() {
     document.getElementById("overlay").style.height = "100%";
 }
 
-function openoverlayorange() {
-    document.getElementById("overlayorange").style.height = "100%";
-}
-
 function closeoverlay() {
     document.getElementById("overlay").style.height = "0%";
-}
-
-function closeoverlayorange() {
-    document.getElementById("overlayorange").style.height = "0%";
 }
