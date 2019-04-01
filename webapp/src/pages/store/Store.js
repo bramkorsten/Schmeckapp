d8552e9c6a1843878b0dafbc38f092be1bbb7e16
Arne Reijntjes <arne123@live.nl>
2019-04-01 13:47:34 +0200

    Added first store version

--- webapp/src/pages/store/Store.js
+++ webapp/src/pages/store/Store.js
@@ -1,5 +1,9 @@
 import React, { PureComponent } from "react";
 import "./Store.css";
+import koffie from "../../images/koffie.jpg";
+import taart from "../../images/taart.jpg";
+import voetbaltafel from "../../images/voetbaltafel.jpg";
+import schmeckle from "../../images/schmeckle.svg";
 
 class Store extends PureComponent {
 
@@ -12,7 +16,19 @@ class Store extends PureComponent {
             <main>
               <div className="store_cards-contain">
                 <div className="store-card-wrap">
+                  <img className="card-wall-img" src={koffie} alt="" />
+                  <div className="card-content">
+                    <h3>Koffie</h3>
+                    <div className="card-amount"><img src={schmeckle} alt="" /><span> 300</span></div>
+                  </div>
+                </div>
 
+                <div className="store-card-wrap">
+                  <img className="card-wall-img" src={taart} alt="" />
+                </div>
+
+                <div className="store-card-wrap">
+                  <img className="card-wall-img" src={voetbaltafel} alt="" />
                 </div>
               </div>
             </main>
