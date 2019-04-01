c562cfdeee866ee83fb4787b82a3ee60f4bec908
Kevin <k.vanhelden@student.fontys.nl>
2019-04-01 10:29:49 +0200

    made progress on the home page

--- webapp/src/pages/home/Home.js
+++ webapp/src/pages/home/Home.js
@@ -3,22 +3,38 @@ import "./Home.css";
 import profilePhoto from "../../images/profilePhoto.jpg";
 
 class Home extends PureComponent {
+  componentWillMount() {
+    this.props.editHeader(true, "");
+  }
 
-    componentWillMount(){
-        this.props.editHeader(true, "");
-    }
+  render() {
+    return (
+      <main>
+        <figure className={"profileInfo"}>
+          <img src={profilePhoto} alt={"profilePicture"} />
+          <figcaption className={"profileName"}>
+            <h1>Sam Koppens</h1>
+          </figcaption>
+        </figure>
 
-    render() {  
-        return (
-            <main>
-                <figure className={"profileInfo"}>
-                    <img src={profilePhoto} alt={"profilePicture"} />
-                    <figcaption className={"profileName"}>
-                        <h1>Sam Koppens</h1>
-                    </figcaption>
-                </figure>
-            </main>
-        );
-    }
+        <div className={"levelContainer"}>
+          <h4>Level 5</h4>
+          <div className={"progressbar-outer"}>
+            <div className={"progressbar-inner"} />
+          </div>
+          <p className={"xpCounter"}>2070/3850 xp</p>
+        </div>
+
+        <section className={"rewardsContainer"}>
+          <h2>Beloningen</h2>
+        </section>
+
+        <section className={"achievementsContainer"}>
+          <h2>Prestaties</h2>
+        </section>
+      </main>
+    );
+  }
 }
+
 export default Home;
